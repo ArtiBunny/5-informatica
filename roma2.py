32464cfae7ff77d4466e7a63d002d0ad8a0a3bcf
Romanchu <90338576+Romanchu@users.noreply.github.com>
2023-03-22 11:51:47 -0300

    Create roma2.py

---
+++ roma2.py
@@ -0,0 +1,6 @@
+setup=[
+    {"processor": "i7 11700k", "ram" : "fury 326B", "gpu" : "rtx 3070Ti", "price" : 1699}
+    {"processor": "ryzen 9 5900x", "ram" : "fury 326B", "gpu" : "Radeon RX6500", "price" : 1899}
+    {"processor": "RYZEN 7 5700g", "ram" : "HyperX 16GB", "gpu" : "Radeon RX6500", "price" : 1199}
+    {"processor": "i5 10400F", "ram" : "HyperX 16GB", "gpu" : "rtx 2060", "price" : 1099}
+]
