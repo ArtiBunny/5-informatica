f2abfd780555ea60b41c5a922806af82a9fd22ca
Máximo Soria <128163353+MaximoSori@users.noreply.github.com>
2023-03-22 11:51:47 -0300

    Create maxi2.py

---
+++ maxi2.py
@@ -0,0 +1,7 @@
+setups = [
+    {"processor": "i7 11700k", "ram": "fury326B", "gpu": "rtx 3070t": , "price": 1699}
+    {"processor": "ryzen 9 590x", "ram": "fury 320B", "gpu": "Radeon Rx 6500", "price": 1899}
+    {"processor": "ryzen 7 5700G", "ram", "hyperx 16GB", "gpu": "Radeon RX 6500", "Price": 1199}
+    {"processor": "i5 10400f", "ram": "hyperx 16gb", "gpu": "RTX 2060", "proce": 1099}
+          ]
+          
